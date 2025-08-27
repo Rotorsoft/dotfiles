889b5051f95a112e9e6303a35e039f7628761572
rotorsoft <rotorsoft@outlook.com>
2025-08-27 16:53:53 -0400

    update wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,38 +1,31 @@
 local wezterm = require("wezterm")
 return {
-	adjust_window_size_when_changing_font_size = false,
-	color_scheme = "Catppuccin Mocha",
-	enable_tab_bar = false,
-	font_size = 16.0,
-	--	font = wezterm.font("JetBrains Mono"),
-	macos_window_background_blur = 30,
-	-- window_background_opacity = 1.0,
-	window_background_opacity = 0.85,
-	-- window_background_opacity = 0.20,
-	window_decorations = "RESIZE",
-	keys = {
-		{
-			key = "q",
-			mods = "CTRL",
-			action = wezterm.action.ToggleFullScreen,
-		},
-		{
-			key = "'",
-			mods = "CTRL",
-			action = wezterm.action.ClearScrollback("ScrollbackAndViewport"),
-		},
-		{
-			key = "F",
-			mods = "CMD", -- CMD = ⌘
-			action = wezterm.action.ToggleFullScreen,
-		},
-	},
-	mouse_bindings = {
-		-- Ctrl-click will open the link under the mouse cursor
-		{
-			event = { Up = { streak = 1, button = "Left" } },
-			mods = "CTRL",
-			action = wezterm.action.OpenLinkAtMouseCursor,
-		},
-	},
+  adjust_window_size_when_changing_font_size = false,
+  color_scheme = "Catppuccin Mocha",
+  enable_tab_bar = false,
+  font_size = 14,
+  font = wezterm.font("JetBrainsMono Nerd Font"),
+  macos_window_background_blur = 20,
+  window_background_opacity = 0.95,
+  window_decorations = "RESIZE",
+  keys = {
+    {
+      key = "'",
+      mods = "CTRL",
+      action = wezterm.action.ClearScrollback("ScrollbackAndViewport"),
+    },
+    {
+      key = "F",
+      mods = "CMD", -- CMD = ⌘
+      action = wezterm.action.ToggleFullScreen,
+    },
+  },
+  mouse_bindings = {
+    -- Ctrl-click will open the link under the mouse cursor
+    {
+      event = { Up = { streak = 1, button = "Left" } },
+      mods = "CTRL",
+      action = wezterm.action.OpenLinkAtMouseCursor,
+    },
+  },
 }
