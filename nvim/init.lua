487aaf29d05dd6c0efd4870d59760ec58dc355f7
rotorsoft <rotorsoft@outlook.com>
2025-09-23 14:39:21 -0400

    Enable zls for zig

--- nvim/init.lua
+++ nvim/init.lua
@@ -38,7 +38,7 @@ vim.pack.add({
   { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 })
 
-vim.lsp.enable({ "lua_ls", "ts_ls" })
+vim.lsp.enable({ "lua_ls", "ts_ls", "zls" })
 
 vim.api.nvim_create_autocmd('VimEnter', {
   callback = function()
